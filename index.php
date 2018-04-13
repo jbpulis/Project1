5161e1083db77d304359933b3644acb5457a0581
Jason Pulis <jasonpulis@mwc-069041.dhcp.missouri.edu>
2018-04-13 16:15:13 -0500

    First commit

---
+++ index.php
@@ -0,0 +1,19 @@
+/*
+Name: Jason Pulis
+Pawprint: jbp2pd
+Date: 28OCT2017
+*/
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Challenge 8</title>
+    </head>
+    <body>
+<?php
+		echo '<img src="images/car.jpeg" alt="car.jpeg"/>';
+?>
+    
+    </body>
+</html>
